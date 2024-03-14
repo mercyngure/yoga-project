9ed39333545bd4643c62b284bf9677aca778fd83
mercy ngure <mercyngure87@gmail.com>
2024-03-14 15:54:56 +0300

    added javascript

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Example percentages (you can replace these with your actual percentages)
-    const htmlProgress = 80;
-    const cssProgress = 60;
-    const jsProgress = 75;
-    const reactProgress = 90;
 
-    // Set progress values
-    document.querySelector('.HTML .progress-line').style.width = `${htmlProgress}%`;
-    document.querySelector('.css .progress-line').style.width = `${cssProgress}%`;
-    document.querySelector('.Javascript .progress-line').style.width = `${jsProgress}%`;
-    document.querySelector('.react .progress-line').style.width = `${reactProgress}%`;
-});
+function showAlert() {
+    var userInput = prompt("Please enter your name:");
+    var userEmail = prompt("Please enter your email:");
+
+
+    if (userInput !== null) {
+        alert("Hello, " + userInput + "! Congratulations! You have successfully registered for free yoga classes.");
+    } else {
+        alert("You cancelled the prompt.");
+    }
+}
+
+document.getElementById("myButton").addEventListener("click", showAlert);
